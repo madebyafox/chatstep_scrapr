f44073657d09ba6e3dfc4a381e633595367e2c02
madebyafox <amyraefox@gmail.com>
2016-05-05 20:58:38 -0700

    update encodings and file order

--- scraper.py
+++ scraper.py
@@ -10,29 +10,31 @@ for filename in os.listdir(inpath):
     soup = BeautifulSoup(open(inpath+filename),"lxml")
     rows = soup.find_all("div",class_='row')
     result = [[]]
-    result [0] = ['message', 'sender', 'time']
+    result [0] = ['time','sender','message']
     
     for r in rows:
         message = r.find(class_="msg")
         if message is not None:
             message = message.get_text()
-            message = message.replace("\n","")
+            message = message.replace("\n","").encode('utf-8')
         else: message = ""
         sender = r.find("span",attrs={'class':'sender'}, recursive = False)
         if sender is not None:
             sender = sender.get_text()
-            sender = sender.replace("\n","")
+            sender = sender.replace("\n","").encode('utf-8')
             sender = sender[:-8]
         else: sender = ""
         time = r.find(class_="timestamp")
         if time is not None:
             time = time.get_text()
-            time = time.replace("\n","")
+            time = time.replace("\n","").encode('utf-8')
         else: time = ""
-        result.append([message,sender,time])
+        result.append([time,sender,message])
     
     ofile = (outpath+filename).replace("html","csv")
     ofile  = open(ofile, "wb")
     writer = csv.writer(ofile, dialect='excel')
     writer.writerows(result)
     ofile.close()
+
+
